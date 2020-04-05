159a39f02dcb5ac074a1463664ad436cb794ace1
Maxwell-Hunt <41925491+Maxwell-Hunt@users.noreply.github.com>
2020-04-05 19:33:54 -0400

    15/15 j5 2015

--- CCC/2015Junior/2015j5.py
+++ CCC/2015Junior/2015j5.py
@@ -1,13 +1,20 @@
 numPieces = int(input())
 numPeople = int(input())
-def numWays(pieces, people, minTake):
+def numWays(pieces, people, minTake = 1, memo = None):
+    if memo == None:
+        memo = {}
     if(people == 1 or people == pieces):
         return 1
     possibleWays = pieces//people
     total = 0
     for i in range(1, possibleWays+1):
         if(i >= minTake):
-            total += numWays(pieces-i, people-1, i)
+            if not (pieces - i, people - 1, i) in memo:
+                x = numWays(pieces-i, people-1, minTake = i, memo = memo)
+                memo[(pieces - i, people - 1, i)] = x
+                total += x
+            else:
+                total += memo[(pieces - i, people - 1, i)]
     return total
 
-print(numWays(numPieces, numPeople, 1))
+print(numWays(numPieces, numPeople))
