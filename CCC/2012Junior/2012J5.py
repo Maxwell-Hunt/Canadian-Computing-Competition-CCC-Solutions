dd7bdb244a3b8e6e7e48484ac899431a25fdb020
Maxwell-Hunt <41925491+Maxwell-Hunt@users.noreply.github.com>
2020-04-05 22:19:34 -0400

    2012 J5 15/15

--- CCC/2012Junior/2012J5.py
+++ CCC/2012Junior/2012J5.py
@@ -1,100 +1,57 @@
-import sys
-sys.setrecursionlimit(1500) # Default 997
-
-PREVIOUS_STATES = []
-
-class Case:
-
-	def __init__(self, number, order):
-		self.number = number
-		self.order = order
-		
-class Node:
-    def __init__(self, case, parent):
-        self.parent = parent
-        self.case = case
-        self.children = []
-
-
-
-def deepCopy(myList):
-	new = []
-	for i in range(len(myList)):
-		new.append(myList[i][:])
-	return new
-
-def ascendingOrder(order):
-	for i in range(len(order)):
-		if(order[i]):
-			if(i + 1 != order[i][0] or len(order[i]) != 1):
+import time
+def ascendingOrder(coins):
+	for i in range(1, len(coins)):
+		if coins[i]:
+			if i + 1 != coins[i][0] or len(coins[i]) != 1:
 				return False
+		else:
+			return False
 	return True
 
-def getPermutations(order):
+def deepcopy(coins):
+	new = []
+	for coin in coins:
+		new.append(coin[:])
+	return new
+
+def getPermutations(coins):
 	permutations = []
-	for i in range(len(order)):
-		if(order[i]):
-			if(i != len(order) - 1):
-				if(not order[i + 1] or order[i][0] < order[i + 1][0]):
-					permutation = deepCopy(order)
-
-					permutation[i + 1].insert(0, permutation[i][0])
-					permutation[i].remove(permutation[i][0])
-					permutations.append(permutation)
-
-
-			if(i != 0):
-				if(not order[i - 1] or order[i][0] < order[i - 1][0]):
-					permutation = deepCopy(order)
-
-					permutation[i - 1].insert(0, permutation[i][0])
-					permutation[i].remove(permutation[i][0])
-					permutations.append(permutation)
-
-			
+	for i in range(len(coins)):
+		if coins[i]:
+			if i < len(coins) - 1:
+				if not coins[i + 1] or coins[i + 1][0] > coins[i][0]:
+					p = deepcopy(coins)
+					p[i] = p[i][1:]
+					tempPlace = [coins[i][0]]
+					tempPlace.extend(coins[i + 1][:])
+					p[i + 1] = tempPlace
+					permutations.append(p)
+			if i > 0:
+				if not coins[i - 1] or coins[i - 1][0] > coins[i][0]:
+					p = deepcopy(coins)
+					p[i] = p[i][1:]
+					tempPlace = [coins[i][0]]
+					tempPlace.extend(coins[i - 1][:])
+					p[i - 1] = tempPlace
+					permutations.append(p)
 	return permutations
 
-def possible(order, depth = 0, queue = None):
-	if(queue == None):
-		queue = []
-		queue.append([0, order])
-	if(ascendingOrder(order)):
-		return depth
-
-	PREVIOUS_STATES.append(order)
-
-	
-	permutations = getPermutations(order)
-
-	for permutation in permutations:
-		if(not permutation in PREVIOUS_STATES):
-			queue.append([depth, permutation])
-	queue.remove(queue[0])
-	if(len(queue) == 0):
-		return False
-
-	check = possible(queue[0][1], queue[0][0] + 1, queue)
-	if(check):
-		pass
-	return check
+def search(configuration):
+	previousStates = []
+	queue = [(configuration, 0)]
+	while queue:
+		if not queue[0][0] in previousStates:
+			previousStates.append(queue[0][0])
+			if ascendingOrder(queue[0][0]):
+				return queue[0][1]
+			queue.extend([(p, queue[0][1] + 1) for p in getPermutations(queue[0][0])])
+		queue.remove(queue[0])
+	return "IMPOSSIBLE"
 
 
 while True:
-	numberOfCoins = int(input())
-	# Break out of loop if number is 0
-	if(not numberOfCoins):
+	numCoins = int(input())
+	if numCoins == 0:
 		break
-	orderOfCoins = input().split()
-	orderOfCoins = [[int(i)] for i in orderOfCoins] # Convert strings in list to integers
-
-	case = Case(numberOfCoins, orderOfCoins)
-	if(ascendingOrder(case.order)):
-		PREVIOUS_STATES = []
-		print(0)
-		continue
-	check = possible(case.order)
-	if(check):
-		print(check) 
-	else:
-		print("IMPOSSIBLE")
-	PREVIOUS_STATES = []
+	initialConfiguration = [[int(coin)] for coin in input().split()]
+	print(search(initialConfiguration))
